f4d9db97e601fe2a79c9a4c6041db2bfd7d28107
XavierVG20 <kjaviervelasque@hotmail.com>
2021-11-18 21:20:28 -0500

    correccion semi final

--- resources/views/partials/mainheader.blade.php
+++ resources/views/partials/mainheader.blade.php
@@ -1,9 +1,9 @@
 <header class="main-header">
 <a href="../../index2.html" class="logo">
       <!-- mini logo for sidebar mini 50x50 pixels -->
-      <span class="logo-mini"><b>S</b>IGF</span>
+      <span class="logo-mini"><b>SI</b>VEN</span>
       <!-- logo for regular state and mobile devices -->
-      <span class="logo-lg"><b>Sistema</b>V</span>
+      <span class="logo-lg"><b>SI</b>Ventas</span>
     </a>
     <!-- Header Navbar: style can be found in header.less -->
     <nav class="navbar navbar-static-top">
@@ -16,8 +16,8 @@
       </a>
       <div class="navbar-custom-menu">
         <ul class="nav navbar-nav">
-          <!-- Messages: style can be found in dropdown.less-->
-          <li class="dropdown messages-menu">
+          <!-- 
+            <li class="dropdown messages-menu">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
               <i class="fa fa-envelope-o"></i>
               <span class="label label-success">4</span>
@@ -25,9 +25,8 @@
             <ul class="dropdown-menu">
               <li class="header">You have 4 messages</li>
               <li>
-                <!-- inner menu: contains the actual data -->
                 <ul class="menu">
-                  <li><!-- start message -->
+                  <li>
                     <a href="#">
                       <div class="pull-left">
                         <img src="{{ asset('img/user2-160x160.jpg') }}" class="img-circle" alt="User Image">
@@ -39,14 +38,16 @@
                       <p>Why not buy a new awesome theme?</p>
                     </a>
                   </li>
-                  <!-- end message -->
                 </ul>
               </li>
               <li class="footer"><a href="#">See All Messages</a></li>
             </ul>
-          </li>
-          <!-- Notifications: style can be found in dropdown.less -->
-          <li class="dropdown notifications-menu">
+          </li>  
+          
+          Messages: style can be found in dropdown.less-->
+        
+          <!-- 
+             <li class="dropdown notifications-menu">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
               <i class="fa fa-bell-o"></i>
               <span class="label label-warning">10</span>
@@ -54,7 +55,7 @@
             <ul class="dropdown-menu">
               <li class="header">You have 10 notifications</li>
               <li>
-                <!-- inner menu: contains the actual data -->
+             inner menu: contains the actual data 
                 <ul class="menu">
                   <li>
                     <a href="#">
@@ -66,6 +67,11 @@
               <li class="footer"><a href="#">View all</a></li>
             </ul>
           </li>
+            
+          
+          
+          Notifications: style can be found in dropdown.less -->
+         
           <li class="dropdown user user-menu">
             <!-- Menu Toggle Button -->
             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
